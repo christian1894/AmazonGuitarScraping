c7163ffa5d4cab694000467d0c3dabf0c49163ef
christian1894 <45642493+christian1894@users.noreply.github.com>
2019-10-07 13:24:37 -0500

    linkGetter

--- amazonGuitarScraping.R
+++ amazonGuitarScraping.R
@@ -9,3 +9,30 @@ href
 
 fullUrl = paste0("www.amazon.com", href)
 fullUrl
+
+
+# https://www.amazon.com/s?k=guitars&page=2&qid=1570470487&ref=sr_pg_2
+# https://www.amazon.com/s?k=guitars&page=3&qid=1570470487&ref=sr_pg_3
+
+library(stringr)
+pag = "https://www.amazon.com/s?k=guitars&page=3&qid=1570470487&ref=sr_pg_3"
+pages_list = c(1:7)
+pag = str_replace(pag, "page=3", paste0("page=", pages_list))
+pag = str_replace(pag, "sr_pg_3", paste0("sr_pg_", pages_list))
+
+linkGetter = function(url){
+  webPage= read_html(url)
+  selector = "div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1) > a:nth-child(1)"
+  hrefNode = html_nodes(webPage, selector)
+  hrefText = html_text(hrefNode)
+  href= html_attr(hrefNode, "href")
+  href
+}
+
+
+links_as_matrix = sapply(pag, linkGetter)
+links_as_vector = as.vector(links_as_matrix)
+
+linksVector = paste0("https://www.amazon.com/", links_as_vector)
+head(linksVector)
+
