824e862ae121f526f965f06b379323288b61f186
Christian Páez <green_pok@hotmail.com>
2019-10-17 14:10:38 -0500

    Preprocessing

---
+++ preprocessing.R
@@ -0,0 +1,22 @@
+books_data_frame = as.data.frame(results)
+#PRICE PROCESSING
+books_data_frame$Price = as.character(books_data_frame$Price)
+books_data_frame$Price = gsub("£", "", books_data_frame$Price)
+books_data_frame$Price = as.numeric(books_data_frame$Price)
+price_avg = mean(books_data_frame$Price, na.rm =TRUE)
+books_data_frame$Price[is.na(books_data_frame$Price)] = price_avg
+hist(books_data_frame$Price)
+summary(books_data_frame$Price)
+
+#PRICE PROCESSING
+books_data_frame$Availability = as.character(books_data_frame$Availability)
+books_data_frame$Availability = gsub(" available)", "", books_data_frame$Availability)
+books_data_frame$Availability = gsub("In stock \\(", "", books_data_frame$Availability)
+books_data_frame$Availability = as.numeric(books_data_frame$Availability)
+availability_avg = mean(books_data_frame$Availability, na.rm=TRUE)
+books_data_frame$Availability[is.na(books_data_frame$Availability)] = availability_avg
+hist(books_data_frame$Availability)
+summary(books_data_frame$Availability)
+
+str(books_data_frame)
+
